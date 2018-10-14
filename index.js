cc762819bf0cb9b4ecedd87b6a2f6113668eb05d
Kevin Chik <kevin.c.chik@gmail.com>
2018-10-14 12:17:41 -0400

    Join and leave messages

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+
 app.use('/public', express.static('public'));
 
 app.get('/', function(req, res) {
@@ -9,16 +10,18 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  id = "xX_hot_girl_69_Xx";
   socket.on('disconnect', function() {
-
+    socket.broadcast.emit('left', username);
   });
-  socket.on('chat message', function(content) {
-    msg = id + ': ' + content
-    if (content != '') io.emit('chat message', msg);
+  socket.on('add', function(username) {
+    socket.username = username;
+    socket.broadcast.emit('joined', username);
+  });
+  socket.on('message', function(msg) {
+    io.emit('message', msg);
   });
 });
 
-http.listen(process.env.PORT || 3000, function(){
-
+http.listen(process.env.PORT || 3000, function() {
+  console.log("Starting server...")
 });
--- public/client.js
+++ public/client.js
@@ -1,12 +1,31 @@
-$(function () {
-  var socket = io();
-  $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
+let socket = io();
+let username;
+
+$('#login-form').submit(function() {
+  username = $('#username').val().trim();
+  if (username != '') socket.emit('add', username);
+  $('#username').val('');
+  return false;
+});
+
+$('#chat-form').submit(function() {
+  msg = $('#m').val().trim();
+  if (msg != '') socket.emit('message', username + ': ' + msg);
+  $('#m').val('');
+  return false;
+});
+
+socket.on('message', function(msg) {
+  $('#scrollable').animate({
+    scrollTop: $('#messages').height()
   });
-  socket.on('chat message', function(msg){
-    $('#scrollable').animate({ scrollTop: $('#messages').height() });
-    $('#messages').append($('<li class="message">').text(msg));
-  });
+  $('#messages').append($('<li class="message">').text(msg));
+});
+
+socket.on('joined', function(username) {
+  $('#messages').append($('<li class="message">').text(username + ' has joined.'));
+});
+
+socket.on('left', function(username) {
+  $('#messages').append($('<li class="message">').text(username + ' has left.'));
 });
