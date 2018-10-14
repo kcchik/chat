1b9b870d5bfa127edd52c72c76c20f479afc92c5
Kevin Chik <kevin.c.chik@gmail.com>
2018-10-13 23:03:07 -0400

    Yes

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
+io.on('connection', function(socket) {
+  id = "000" + Math.floor(Math.random() * 1000);
+  id = id.substr(id.length - 4);
+  socket.on('disconnect', function() {
     console.log('user disconnected');
   });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(content) {
+    msg = id + ': ' + content
+    if (content != '') io.emit('chat message', msg);
   });
 });
 
